847df9efc1e2cf3b1e3751e4de8376e4d1714bc6
Erik Svensson <eriksven@Eriks-Mac-mini.local>
2019-04-30 16:52:43 +0200

    feat: jungle beat in demo ensemble

--- lib/magic_flute/demo_ensemble/kick_player.ex
+++ lib/magic_flute/demo_ensemble/kick_player.ex
@@ -3,7 +3,7 @@ defmodule MagicFlute.DemoEnsemble.KickPlayer do
 
   use MagicFlute.Player, instrument: {:percussive, 5}
 
-  def read_notes(_bar, beat) when beat == 1 do
+  def read_notes(bar, beat) when rem(beat, 4) == 0 or bar == 1 do
     [
       ~n(c-1 10 mf)
     ]
--- lib/magic_flute/demo_ensemble/snare_player.ex
+++ lib/magic_flute/demo_ensemble/snare_player.ex
@@ -3,9 +3,33 @@ defmodule MagicFlute.DemoEnsemble.SnarePlayer do
 
   use MagicFlute.Player, instrument: {:percussive, 6}
 
-  def read_notes(_bar, beat) when beat == 9 do
+  def read_notes(_bar, beat) when rem(beat, 4) == 0 do
     [
-      ~n(c3 10 mf)
+      ~n(c3 10 mp)
+    ]
+  end
+
+  def read_notes(_bar, beat) when rem(beat, 4) == 1 do
+    [
+      ~n(f3 10 p)
+    ]
+  end
+
+  def read_notes(_bar, beat) when rem(beat, 4) == 2 do
+    [
+      ~n(a3 10 p)
+    ]
+  end
+
+  def read_notes(_bar, beat) when rem(beat, 4) == 3 do
+    [
+      ~n(a5 10 mf)
+    ]
+  end
+
+  def read_notes(_bar, beat) when rem(beat, 4) == 4 do
+    [
+      ~n(b3 10 p)
     ]
   end
 end
